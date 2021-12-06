e59891cf5418311bd53f7ce86886a59528d666e6
edunaev <dunaeve@yahoo.com>
2021-12-06 20:32:50 +0100

    step 3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,10 +1,13 @@
 package hexlet.code;
 
 
+import static hexlet.code.Cli.doSomeStaff;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        //new Start().play();
+        doSomeStaff();
 
     }
 }
